5dd0712bcaf85d101bcd63f6a911fd4bd2ca6007
Hao Ying Liang <hyl0327@gmail.com>
2016-02-23 11:48:00 +0800

    Changed comment to lowercase

--- web/esoe_oop_judge/settings.py
+++ web/esoe_oop_judge/settings.py
@@ -113,8 +113,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-# We intentionally disable timezone support in order for users to see exactly
-# same datetime information from everywhere.
+# we intentionally disable timezone support in order for users to see exactly
+# same datetime information from everywhere
 USE_TZ = False
 
 
