fa6d49060b81714dc3b5560fee57afafeda62860
Hao Ying Liang <hyl0327@gmail.com>
2016-03-06 21:41:29 +0800

    Add STATIC_ROOT

--- web/esoe_oop_judge/settings.py
+++ web/esoe_oop_judge/settings.py
@@ -135,6 +135,7 @@ DATETIME_FORMAT='Y-m-d, H:i:s'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(config.WEB_DIR, 'static')
 STATICFILES_DIRS = [config.JUDGE_STATIC_DIR]
 
 
