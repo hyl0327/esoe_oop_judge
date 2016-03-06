66ac648e347b815c7acdaccd9a9aa933e6ef17b8
Hao Ying Liang <hyl0327@gmail.com>
2016-03-07 04:27:09 +0800

    Deploy

--- web/esoe_oop_judge/settings.py
+++ web/esoe_oop_judge/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^1&vzcf@^j8&lyaxs1hus6k69=aw4$x*#&794g&abj)=be6(gr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['v2.javajudge.cklab.org']
 
 
 # Application definition
