f7dcaa8261cd52171a66bbee06bea04136c244c4
zon <zon5566@github.com>
2016-03-07 12:20:37 +0800

    Add messages style in base.html and some bootstrap icon.

--- web/judge/static/judge/js/base.js
+++ web/judge/static/judge/js/base.js
@@ -1,6 +1,7 @@
-function close_message(){
-	console.log('testets');
-	if(document.getElementById("messages_div").contains(ul)){
-		document.getElementById("messages_ul").className += 'faded';
-	}
+function close_message(index){
+	var li = document.getElementById("close_li"+index);
+	var btn = document.getElementById("close"+index);
+	console.log("close_li"+index);
+	li.style.display="none";
+	btn.style.display="none";
 }
