ea6252e69b414e000ccf443f48d3d324b4056ae3
Hao Ying Liang <hyl0327@gmail.com>
2016-06-05 14:29:36 +0800

    utilities/add_users.py now reads input from stdin

--- utilities/add_users.py
+++ utilities/add_users.py
@@ -12,28 +12,22 @@ from judge.models import Profile
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('usage: {} filename'.format(sys.argv[0]),
-              file=sys.stderr)
-        sys.exit(1)
+    for line in sys.stdin:
+        student_id, name = line.split(',')
 
-    with open(sys.argv[1]) as f:
-        for line in f:
-            student_id, name = line.split(',')
+        # wipe out the trailing '\n'
+        name = name[:-1]
 
-            # wipe out the trailing '\n'
-            name = name[:-1]
+        # create user, with its password being the same as its student ID,
+        # and its email being its NTU mail
+        user = User.objects.create_user(username=student_id,
+                                        email=(student_id + '@ntu.edu.tw'),
+                                        password=student_id)
+        user.save()
 
-            # create user, with its password being the same as its student ID,
-            # and its email being its NTU mail
-            user = User.objects.create_user(username=student_id,
-                                            email=(student_id + '@ntu.edu.tw'),
-                                            password=student_id)
-            user.save()
-
-            # create its corresponding profile
-            profile = Profile(user=user, name=name)
-            profile.save()
+        # create its corresponding profile
+        profile = Profile(user=user, name=name)
+        profile.save()
 
 if __name__ == '__main__':
     main()
